45898b0908f13b6d5f3574544753d7dcbf145157
yzl <fjt086@gmail.com>
2022-06-06 15:04:44 -0700

    0.0

---
+++ ResumeManagement/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ResumeManagement project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResumeManagement.settings')
+
+application = get_asgi_application()
